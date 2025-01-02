3c0a7f893ecee9deee1021450c9e7fab0f854bf5
Mustafa <mstf.fdn@outlook.com>
2025-01-03 02:17:58 +0300

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -145,12 +145,12 @@ const Header = () => {
           </p>
         </div>
         <div
-  className={`w-[10rem] h-[3rem] ${darkMode ? "bg-pink-500" : "bg-pink-600"} rounded-l-3xl mx-auto`}
+  className={`w-[5rem] h-[3rem] ${darkMode ? "bg-pink-500" : "bg-pink-600"} rounded-l-3xl mx-auto`}
   style={{
     position: "relative", // Sabit konumda tutmak için fixed
     top: "10%", // Sayfanın üst kısmına göre mesafe
     left: "50%", // Sayfanın tam ortasında
-    transform: "translateX(-25%) translateY(-500%)", // Tam ortada olması için
+    transform: "translateX(0%) translateY(-500%)", // Tam ortada olması için
     marginTop: "-1%",
     zIndex: 10, // Diğer öğelerin üstünde görünmesini sağlamak için
   }}
