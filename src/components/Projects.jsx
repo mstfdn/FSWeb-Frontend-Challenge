588900acc09e8a3d80ecc2c07da1163ecd8d91aa
Mustafa <mstf.fdn@outlook.com>
2025-01-03 04:01:00 +0300

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,18 +3,20 @@ import { LanguageContext } from "../context/LanguageContext";
 import projectsData from '../dataJSON/projectsData.json';
 import projectPhoto from "../assets/pizza.png";
 import projectPhoto2 from "../assets/proje2.png";
+import { useTheme } from "../context/ThemeContext";
 
 function Projects() {
   const [projects, setProjects] = useState([]);
   const { language } = useContext(LanguageContext);
+  const { darkMode } = useTheme(); // Dark mode durumunu alıyoruz
 
   useEffect(() => {
     setProjects(projectsData);
   }, []);
 
   return (
-    <section className="py-16">
-      <h4 className="text-4xl font-semibold text-center text-blue-700 dark:text-white mb-10">
+    <section className={`py-16 ${darkMode ? 'bg-gray-900 text-white' : 'bg-white'}`}>
+      <h4 className={`text-4xl font-semibold text-center ${darkMode ? 'text-blue-300' : 'text-blue-700'} mb-10`}>
         {language === "en" ? "Projects" : "Projeler"}
       </h4>
       <div className="flex flex-wrap justify-center gap-10">
@@ -26,7 +28,7 @@ function Projects() {
             return (
               <div
                 key={project.id}
-                className="bg-gradient-to-r from-blue-100 to-blue-300 shadow-lg rounded-lg w-[350px] overflow-hidden transform transition-all duration-300 hover:scale-105"
+                className={`bg-gradient-to-r from-blue-100 to-blue-300 shadow-lg rounded-lg w-[350px] overflow-hidden transform transition-all duration-300 hover:scale-105 ${darkMode ? 'bg-gray-800' : ''}`}
               >
                 <div className="relative h-64">
                   <img
@@ -36,11 +38,11 @@ function Projects() {
                   />
                   <div className="absolute inset-0 bg-black opacity-30"></div>
                 </div>
-                <div className="p-6 bg-white dark:bg-gray-700 rounded-b-lg">
-                  <h5 className="text-xl font-semibold text-blue-800 dark:text-white">
+                <div className={`p-6 ${darkMode ? 'bg-gray-700' : 'bg-white'} rounded-b-lg`}>
+                  <h5 className={`text-xl font-semibold ${darkMode ? 'text-white' : 'text-blue-800'}`}>
                     {language === "en" ? project["title-en"] : project["title-tr"]}
                   </h5>
-                  <p className="text-sm text-gray-600 dark:text-gray-300 mt-2">
+                  <p className={`text-sm ${darkMode ? 'text-gray-300' : 'text-gray-600'} mt-2`}>
                     {language === "en"
                       ? project["description-en"]
                       : project["description-tr"]}
@@ -57,7 +59,7 @@ function Projects() {
                   </div>
                   <div className="flex justify-between mt-6">
                     <a
-                      className="text-blue-700 font-semibold hover:underline dark:text-white"
+                      className={`text-blue-700 font-semibold hover:underline ${darkMode ? 'text-white' : ''}`}
                       href={project.githubLink}
                       target="_blank"
                       rel="noopener noreferrer"
@@ -65,7 +67,7 @@ function Projects() {
                       {language === "en" ? "GitHub" : "GitHub"} &rarr;
                     </a>
                     <a
-                      className="text-blue-700 font-semibold hover:underline dark:text-white"
+                      className={`text-blue-700 font-semibold hover:underline ${darkMode ? 'text-white' : ''}`}
                       href={project.appLink}
                       target="_blank"
                       rel="noopener noreferrer"
